f0b73f5436f14818e5d5528033c2940e8292a011
srin <smadipalli@hotmail.com>
2015-04-21 22:10:45 +0100

    update mailer config2

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
 	address: "smtp.mandrillapp.com",
 	port: 587,
 	enable_starttls_auto: true,
-	user_name: ENV["MANDRILL_USERNAME"],
-	password: ENV["MANDRILL_PASSWORD"],
+	user_name: "MANDRILL_USERNAME",
+	password: "MANDRILL_PASSWORD",
 	authentication: "login",
 }
 
